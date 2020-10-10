02c7b5f2c37628e0e9dc24e6513072eddf81be7c
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-09 21:21:25 -0700

    added questions

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# READMEgenerator
-a terminal based README generator
+
+# this is my title
+
+
+## Installation
+
+how do i install?
+<hr>
+## Description
+
+ehre is my lengthy description of my project
+<hr>
+
+## Usage
+
+USAGE
+<hr>
+## License
+
+b
+
+<hr>
+# Author
+
+Jen Henry
+GitHub: jenryhennifer
+[my photo](https://avatars.githubusercontent.com/jenryhennifer.png?size=50)
+
+
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const inquirer = require("inquirer")
+const axios = require('axios')
 const fs = require("fs")
 const util = require("util")
 
-// const questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// ];
-
-function promptQuestions(){
-    inquirer
+function questions(){
+    return inquirer
         .prompt([
-            {
+
+        {
+            type: 'input',
+            name: 'nameOfPerson',
+            message: 'What is your first/last name?'
+
+        },
+        {
             type: 'input',
             name: 'username',
             message: 'GitHub Username:'
@@ -17,12 +23,12 @@ function promptQuestions(){
         {
             type: 'input',
             name: 'title',
-            message: 'Title: '
+            message: 'Title of Project: '
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Description of Project: '
+            message: 'Describe your project: '
         },
         {
             type: 'input',
@@ -35,9 +41,10 @@ function promptQuestions(){
             message: 'Usage: '
         },
         {
-            type: '', //possibly give options
-            name: 'liscence',
-            message: ''
+            type: 'list', //possibly give options
+            name: 'license',
+            message: 'What license would you like to use?',
+            choices: ['MIT','apache','unlicense'] 
         },
         {
             type: 'input',
@@ -50,18 +57,129 @@ function promptQuestions(){
             message: 'Tests: '
         },
 
+
         ])
 }
 
+const licenses = (response) => {
+    const options = {
+    MIT: `MIT License
 
-function writeToFile(fileName, data) {
+    Copyright (c) 2020 ${response.nameOfPerson}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.`,
 
+    apache: `Apache
+    
+    Copyright 2020 ${response.nameOfPerson}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`,
+
+    unlicense: `Unlicense
+    
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+    
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+    For more information, please refer to <http://unlicense.org/>`
+    }
+    return options[response.license];
+};
+
+
+
+function generateMD(response){
+    return `
+# ${response.title}
+
+
+## Installation
+
+${response.installation}
+<hr>
+
+## Description
+
+${response.description}
+<hr>
+
+## Usage
+
+${response.usage}
+<hr>
+
+## License
+
+${licenses(response)}
+
+<hr>
+
+# Author
+
+${response.nameOfPerson}
+
+GitHub: ${response.username}
+
+![my photo](https://github.com/${response.username}.png?size=200)
+
+`
 }
 
-function init() {
 
-}
+questions()
+  .then(function(response) {
+    const md = generateMD(response);
 
-init();
-
-promptQuestions();
+    return writeFileAsync("READMEsample.md", md);
+  })
+  .then(function() {
+    console.log("Successfully wrote");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
